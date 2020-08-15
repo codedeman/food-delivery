dcdea823c1807c5a12ada79866a5918ac2d3f5ce
Kienpt1 <kienpt1@vnpay.vn>
2020-08-15 18:17:29 +0700

    what thee fuck

---
+++ lib/models/food.dart
@@ -0,0 +1,31 @@
+
+class Food {
+
+
+
+}
+
+
+//final DocumentReference reference;
+//
+//Record.fromMap(Map<String, dynamic> map, {this.reference})
+//
+//: assert(map['name'] != null),
+//assert(map['votes'] != null),
+//assert(map['phone'] != null),
+//assert(map['station'] != null),
+////        assert(map['destination'] != null),
+//
+//name = map['name'],
+//votes = map['votes'],
+//phone =  map['phone'],
+//station = map['station'],
+//destination = map['destination'];
+//
+//Record.fromSnapshot(DocumentSnapshot snapshot)
+//: this.fromMap(snapshot.data, reference: snapshot.reference);
+//
+//@override
+//String toString() => "Record<$name:$votes:$phone:$station:$destination>";
+//}
+//
